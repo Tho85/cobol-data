cf856c5a98df9d151482239624bd2b6038e77f63
Thomas Hollstegge <thomas.hollstegge@zweitag.de>
2012-11-17 19:52:35 +0100

    Parse levels

--- lib/cobol_data/schema_parser.rb
+++ lib/cobol_data/schema_parser.rb
@@ -3,47 +3,49 @@ class CobolData::SchemaParser
     TYPES = { 'X' => :text, '9' => :number }
 
     def parse_line(s)
-      # Check for standard format
-      if line_match = %r|(?<name>[\w\-]+)\s+PIC\s+(?<format>\S*)\s*(?<modifiers>.*)|.match(s)
-        # Expand length in parentheses
-        format_expanded = line_match[:format].gsub(/([X9])\((\d+)\)/) { $1 * $2.to_i }
+      # Parse level, name and definition
+      if line_match = %r|(?<level>\d+)\s+(?<name>[\w\-]+)\s+(?<definition>.*)|.match(s)
+        format = { level: line_match[:level].to_i }
 
-        # Check for general string or number format
-        format = if format_match = %r|^(?<sign>[S]?)(?<type>[X9]+)(?<scale>.*)|.match(format_expanded)
-          { type: TYPES[format_match[:type][0]], length: format_match[:type].length }
-        end
+        # Check for standard format
+        if definition_match = %r|PIC\s+(?<format>\S*)\s*(?<modifiers>.*)|.match(line_match[:definition])
+          # Expand length in parentheses
+          format_expanded = definition_match[:format].gsub(/([X9])\((\d+)\)/) { $1 * $2.to_i }
 
-        # Is a scale given?
-        if format_match[:scale]
-          # Check for decimals, e.g. 9(4)V99
-          if scale_match = %r|^V(?<scale>\d+)|.match(format_match[:scale])
-            format.merge! scale: scale_match[:scale].length
+          # Check for general string or number format
+          if format_match = %r|^(?<sign>[S]?)(?<type>[X9]+)(?<scale>.*)|.match(format_expanded)
+            format.merge! type: TYPES[format_match[:type][0]], length: format_match[:type].length
           end
-        end
 
-        # Is there a sign?
-        if format_match[:sign] == 'S'
-          format.merge! signed: true
-        end
-
-        # Are there some modifiers?
-        if line_match[:modifiers]
-          # Check for COMP-3
-          if modifier_match = %r|COMP-3$|.match(line_match[:modifiers])
-            format.merge! packed: true
+          # Is a scale given?
+          if format_match[:scale]
+            # Check for decimals, e.g. 9(4)V99
+            if scale_match = %r|^V(?<scale>\d+)|.match(format_match[:scale])
+              format.merge! scale: scale_match[:scale].length
+            end
           end
+
+          # Is there a sign?
+          if format_match[:sign] == 'S'
+            format.merge! signed: true
+          end
+
+          # Are there some modifiers?
+          if definition_match[:modifiers]
+            # Check for COMP-3
+            if modifier_match = %r|COMP-3$|.match(definition_match[:modifiers])
+              format.merge! packed: true
+            end
+          end
+
+          # Check for redefines
+        elsif definition_match = %r|REDEFINES\s+(?<original>[\w\-]+).*|.match(s)
+          format.merge! redefines: to_field_name(definition_match[:original])
         end
 
         { to_field_name(line_match[:name]) => format }
 
-      # Check for redefines
-      # TODO: DRY name part of regex (see above)
-      elsif line_match = %r|(?<name>[\w\-]+)\s+REDEFINES\s+(?<original>[\w\-]+).*|.match(s)
-        { to_field_name(line_match[:name]) => { redefines: to_field_name(line_match[:original]) } }
       end
-    rescue
-      # TODO Raise ParserException
-      nil
     end
 
     def parse(s)
--- spec/cobol_data/schema_parser_spec.rb
+++ spec/cobol_data/schema_parser_spec.rb
@@ -4,49 +4,53 @@ describe CobolData::SchemaParser do
   context 'single line' do
     context 'standard format' do
       it 'parses strings' do
-        "05 NAMEALT PIC XXX".should parse_line_to namealt: { type: :text, length: 3 }
+        "05 NAMEALT PIC XXX".should parse_line_to namealt: { type: :text, length: 3, level: 5 }
       end
 
       it 'parses numbers' do
-        "05 NUMALT PIC 999".should parse_line_to numalt: { type: :number, length: 3 }
+        "05 NUMALT PIC 999".should parse_line_to numalt: { type: :number, length: 3, level: 5 }
       end
 
       it 'parses decimals' do
-        "04 DEC2 PIC 999V99".should parse_line_to dec2: { type: :number, length: 3, scale: 2 }
+        "04 DEC2 PIC 999V99".should parse_line_to dec2: { type: :number, length: 3, scale: 2, level: 4 }
       end
 
     end
 
     context 'repeat factors' do
       it 'works for strings' do
-        "05 NAME PIC X(10)".should parse_line_to name: { type: :text, length: 10 }
+        "05 NAME PIC X(10)".should parse_line_to name: { type: :text, length: 10, level: 5 }
       end
 
       it 'works for numbers' do
-        "05 NUM PIC 9(5)".should parse_line_to num: { type: :number, length: 5 }
+        "05 NUM PIC 9(5)".should parse_line_to num: { type: :number, length: 5, level: 5 }
       end
 
       it 'works for decimals' do
-        "04 DEC PIC 9(4)V99".should parse_line_to dec: { type: :number, length: 4, scale: 2 }
+        "04 DEC PIC 9(4)V99".should parse_line_to dec: { type: :number, length: 4, scale: 2, level: 4 }
+      end
+
+      it 'works for decimals scale' do
+        "04 DECC PIC 9(5)V9(4)".should parse_line_to decc: { type: :number, length: 5, scale: 4, level: 4 }
       end
 
     end
     context 'modifiers' do
       it 'parses packed decimals' do
-        "04 COMP PIC 9(5)V9  COMP-3".should parse_line_to comp: { type: :number, length: 5, scale: 1, packed: true }
+        "04 COMP PIC 9(5)V9  COMP-3".should parse_line_to comp: { type: :number, length: 5, scale: 1, packed: true, level: 4 }
       end
 
       it 'parses signs' do
-        "04 NUMSIGNED PIC S9(5)".should parse_line_to numsigned: { type: :number, length: 5, signed: true }
+        "04 NUMSIGNED PIC S9(5)".should parse_line_to numsigned: { type: :number, length: 5, signed: true, level: 4 }
       end
     end
 
     it 'parses variable names correctly' do
-      "05 NAME-WITH-DASH PIC X(10)".should parse_line_to name_with_dash: { type: :text, length: 10 }
+      "05 NAME-WITH-DASH PIC X(10)".should parse_line_to name_with_dash: { type: :text, length: 10, level: 5 }
     end
 
     it 'parses redefines' do
-      "04 REDEF REDEFINES ORIGINAL".should parse_line_to redef: { redefines: :original }
+      "04 REDEF REDEFINES ORIGINAL".should parse_line_to redef: { redefines: :original, level: 4 }
     end
   end
 
@@ -56,22 +60,22 @@ describe CobolData::SchemaParser do
          01 LASTNAME PIC X(10).
          01 ACCOUNTNUMBER PIC 9(5).
       |.should parse_to({
-        firstname: { type: :text, length: 10 },
-        lastname: { type: :text, length: 10 },
-        accountnumber: { type: :number, length: 5 }
+        firstname: { type: :text, length: 10, level: 1 },
+        lastname: { type: :text, length: 10, level: 1 },
+        accountnumber: { type: :number, length: 5, level: 1 }
       })
     end
 
     it 'ignores comments' do
       %q|* This is a comment
          01 FIRSTNAME PIC X(10).
-         |.should parse_to firstname: { type: :text, length: 10 }
+         |.should parse_to firstname: { type: :text, length: 10, level: 1 }
     end
 
     it 'allows multi-line definitions' do
       %q|01 FIRSTNAME
             PIC X(10).
-        |.should parse_to firstname: { type: :text, length: 10 }
+        |.should parse_to firstname: { type: :text, length: 10, level: 1 }
     end
   end
 end
