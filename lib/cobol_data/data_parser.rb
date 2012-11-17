b20c4a69eaa999864e3df8e9fef07bcd5d7cd2f2
Thomas Hollstegge <thomas.hollstegge@zweitag.de>
2012-11-17 19:52:50 +0100

    Parse redefined data correctly

--- lib/cobol_data/data_parser.rb
+++ lib/cobol_data/data_parser.rb
@@ -7,10 +7,10 @@ class CobolData::DataParser
 
   def parse_line(line)
     # First convert schema to string unpack schema
-    raw_hash = Hash[@schema.keys.zip(line.unpack(schema_to_unpack_argument))]
+    raw_hash = Hash[prepared_schema.keys.zip(line.unpack(schema_to_unpack_argument))]
 
     # Then convert data types
-    @schema.inject({}) do |hash, (field_name, field_format)|
+    prepared_schema.inject({}) do |hash, (field_name, field_format)|
       # Fetch raw value from hash
       raw_value = raw_hash[field_name]
 
@@ -34,9 +34,25 @@ class CobolData::DataParser
 
   private
   def schema_to_unpack_argument
-    @schema.values.map do |field_format|
+    prepared_schema.values.map do |field_format|
       "a#{field_format[:length]}"
     end.join("")
+  end
+
+  def prepared_schema
+    # Prepare schema by removing redefined columns
+    prepared_schema = @schema.dup
+
+    @schema.each do |(field_name, field_format)|
+      if field_format[:redefines]
+        # Simply delete redefined fields...
+        prepared_schema.delete field_format[:redefines]
+        # ... and delete redefining field
+        prepared_schema.delete field_name
+      end
+    end
+
+    prepared_schema
   end
 
 end
--- spec/cobol_data/data_parser_spec.rb
+++ spec/cobol_data/data_parser_spec.rb
@@ -14,4 +14,22 @@ describe CobolData::DataParser do
       parser.parse_line("015THOMAS    0180").should == { id: 15, name: "THOMAS    ", height: 1.8 }
     end
   end
+
+  context 'with redefines' do
+    let(:schema) { {
+      id:         { level: 1, type: :number, length: 4 },
+      name:       { level: 1, type: :text,   length: 20 },
+      real_name:  { level: 1, redefines: :name },
+      first_name: { level: 2, type: :text,   length: 10 },
+      last_name:  { level: 2, type: :text,   length: 10 },
+      number:     { level: 1, type: :number, length: 2}
+    } }
+
+    let(:parser) { CobolData::DataParser.new(schema) }
+
+    it 'redefines correctly' do
+      parser.parse_line("1234VORNAME   NACHNAME  42").should == { id: 1234, first_name: 'VORNAME   ', last_name: 'NACHNAME  ', number: 42 }
+    end
+
+  end
 end
